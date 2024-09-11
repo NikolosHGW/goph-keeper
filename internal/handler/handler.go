e3c9c95d37a3f386c1abb0eae273db6b95ea08c5
Nikita Bakarev <nic231731@yandex.ru>
2024-09-11 19:38:45 +0300

    test: create router test

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,5 +1,11 @@
 package handler
 
+import "net/http"
+
+type authHandler interface {
+	RegisterUser(http.ResponseWriter, *http.Request)
+}
+
 type Handlers struct {
-	AuthHandler *AuthHandler
+	AuthHandler authHandler
 }
---
+++ internal/infrastructure/router/router_test.go
@@ -0,0 +1,59 @@
+package router
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/NikolosHGW/goph-keeper/internal/handler"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockAuthHandler struct {
+	mock.Mock
+}
+
+func (m *MockAuthHandler) RegisterUser(w http.ResponseWriter, r *http.Request) {
+	m.Called(w, r)
+	w.WriteHeader(http.StatusOK)
+}
+
+func TestNewRouter_RegisterRoute(t *testing.T) {
+	mockAuthHandler := new(MockAuthHandler)
+
+	mockAuthHandler.On("RegisterUser", mock.Anything, mock.Anything).Return()
+
+	handlers := &handler.Handlers{
+		AuthHandler: mockAuthHandler,
+	}
+
+	r := NewRouter(handlers)
+
+	req, _ := http.NewRequest(http.MethodPost, "/api/user/register", nil)
+	rr := httptest.NewRecorder()
+
+	r.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+	mockAuthHandler.AssertCalled(t, "RegisterUser", mock.Anything, mock.Anything)
+}
+
+func TestNewRouter_UnknownRoute(t *testing.T) {
+	mockAuthHandler := new(MockAuthHandler)
+
+	mockAuthHandler.On("RegisterUser", mock.Anything, mock.Anything).Return()
+
+	handlers := &handler.Handlers{
+		AuthHandler: mockAuthHandler,
+	}
+
+	r := NewRouter(handlers)
+
+	req, _ := http.NewRequest(http.MethodGet, "/unknown", nil)
+	rr := httptest.NewRecorder()
+
+	r.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusNotFound, rr.Code)
+}
